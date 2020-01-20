9df27d75070cf990e9875381c46fb3e496bae8ef
yangqing <>
2020-01-20 13:47:09 +0800

    golang

--- encryption/main.go
+++ encryption/main.go
@@ -1,6 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"golab/encryption/sign"
+)
 
+func main() {
+	src := "hello world"
+	key := "123456"
+
+	fmt.Println(sign.Md5Str(src))
+	fmt.Println(sign.HmacSha1(src, key))
+	fmt.Println(sign.HmacSha256(src, key))
+	fmt.Println(sign.HmacSha512(src, key))
+	fmt.Println(sign.Sha1str(src))
+	fmt.Println(sign.Sha256str(src))
+	fmt.Println(sign.Sha512str(src))
+	fmt.Println(sign.Base64EncodeStr(src))
+	fmt.Println(sign.Base64DecodeStr(sign.Base64EncodeStr(src)))
 
 }
